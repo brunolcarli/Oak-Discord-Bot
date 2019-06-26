c947b4f478a6bca61c3fb1da3090bb3ae620fec3
Bruno Luvizotto Carli <bruno.carli@smartgreen.net>
2019-06-26 14:15:25 -0300

    ADD lisa url aos setings

--- settings.py
+++ settings.py
@@ -10,3 +10,5 @@ POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 ITEM_API_URL = 'https://pokeapi.co/api/v2/item/'
 ABILITY_API_URL = 'https://pokeapi.co/api/v2/ability/'
 EFFECTIVENESS_API_URL = 'https://raw.githubusercontent.com/pokeweak/pokeweak.github.io/master/pokemon.json'
+
+LISA_URL = 'https://Lisa-API--brunolcarli.repl.co/graphql/'
